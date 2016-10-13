22298f0a5d46f6e24898c871c8b4d6858f8b1240
Takashiro <takashiro@qq.com>
2016-10-13 09:03:59 +0800

    添加往来单位名称的索引

--- module/user/install.sql
+++ module/user/install.sql
@@ -2,6 +2,8 @@
 ALTER TABLE `pre_user` ADD `deleted` tinyint(1) NOT NULL DEFAULT '0';
 ALTER TABLE `pre_user` ADD KEY `deleted` (`deleted`);
 
+ALTER TABLE `pre_user` ADD KEY `nickname` (`nickname`);
+
 CREATE TABLE `pre_useracronym` (
 	`id` mediumint(8) unsigned NOT NULL,
 	`nickname` varchar(50) DEFAULT NULL,
